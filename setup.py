375f146a38d984713a2ee714cd30cfa5d84e6cfb
Jostein Blyverket <josteinbl@met.no>
2025-03-06 14:09:08 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,4 @@ setup(
     name="obsOp",
     version="1.0",
     packages=find_packages(),
-    entry_points = {
-              'console_scripts': [
-                  'prediction_data = Training_data.Training_data_static:main',
-                  'GNN = Train_model_and_make_predictions.Predictions:main',
-              ],
-      },
 )
